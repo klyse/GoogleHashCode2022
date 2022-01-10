86a306f23324231cd5c1e0f1c79f52b193114c99
klyse <Klaus.Pruenster@gmail.com>
2022-01-10 14:09:38 +0100

    fix data path

--- GoogleHashCode/EnvironmentConstants.cs
+++ GoogleHashCode/EnvironmentConstants.cs
@@ -1,14 +1,25 @@
-﻿using System.IO;
-using GoogleHashCode.Base;
-using GoogleHashCode.Model;
+﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace GoogleHashCode;
 
 public static class EnvironmentConstants
 {
-    // ReSharper disable PossibleNullReferenceException
-    public static string DataPath => Path.Combine(new FileInfo(typeof(ISolver<bool, Output>).Assembly.Location).Directory.Parent.Parent.Parent.Parent.ToString(), "Environment");
-    // ReSharper restore PossibleNullReferenceException
+    private static readonly Lazy<string> DataPathLazy = new(() => Path.Combine(TryGetSolutionDirectoryInfo().FullName, "Environment"));
+    public static string DataPath => DataPathLazy.Value;
+
+    private static DirectoryInfo TryGetSolutionDirectoryInfo(string currentPath = null)
+    {
+
+        var directory = new DirectoryInfo(
+            currentPath ?? Directory.GetCurrentDirectory());
+        while (directory != null && !directory.GetFiles("*.sln").Any())
+        {
+            directory = directory.Parent;
+        }
+        return directory;
+    }
 
     public static string InputPath => Path.Combine(DataPath, "Input");
     public static string OutputPath => Path.Combine(DataPath, "Output");
