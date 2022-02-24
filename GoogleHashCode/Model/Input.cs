76451d4cedd6f5207c98d7ff53c3c5700101983d
klyse <Klaus.Pruenster@gmail.com>
2022-02-24 19:07:44 +0100

    models3

--- GoogleHashCode/Model/Input.cs
+++ GoogleHashCode/Model/Input.cs
@@ -16,14 +16,26 @@ public record Input(List<Contributor> Contributors, List<Project> Projects)
         var contributors = first[0];
         var projects = first[1];
 
-        var cnt = 0;
-        foreach (var row in values.Skip(1))
+        var contributorsList = new List<Contributor>();
+        var projectsList = new List<Project>();
+
+        for (int i = 1; i < contributors; i++)
         {
-            var splitRow = row.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            var splitRow = values[i].Split(" ", StringSplitOptions.RemoveEmptyEntries);
             var name = splitRow[0];
             var count = int.Parse(splitRow[1]);
 
-            cnt++;
+            var skillsList = new List<Skills>();
+            for (int y = i + 1; y < i + count; y++)
+            {
+                var splitRow1 = values[i].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var skillName = splitRow1[0];
+                var level = int.Parse(splitRow1[1]);
+                skillsList.Add(new Skills(skillName, level));
+            }
+            contributorsList.Add(new Contributor(name, skillsList));
         }
+
+        return new Input(contributorsList, projectsList);
     }
 }
