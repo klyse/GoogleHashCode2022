9edad73b1b709f8e10ef7c9892d61109f12b38e2
klyse <Klaus.Pruenster@gmail.com>
2022-02-24 19:00:30 +0100

    models 3

--- GoogleHashCode/Model/Input.cs
+++ GoogleHashCode/Model/Input.cs
@@ -13,16 +13,17 @@ public record Input(List<Contributor> Contributors, List<Project> Projects)
     public static Input Parse(string[] values)
     {
         var first = values.First().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
-        var contributors = first.First();
-        var projects = first.Skip(1).First();
-        
+        var contributors = first[0];
+        var projects = first[1];
+
+        var cnt = 0;
         foreach (var row in values.Skip(1))
         {
             var splitRow = row.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
             var name = splitRow[0];
             var count = int.Parse(splitRow[1]);
-            
-            
+
+            cnt++;
         }
     }
 }
