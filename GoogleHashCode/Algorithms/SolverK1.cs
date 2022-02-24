40b4a3ca0bc2e879f3cdc938b599534e862fc6e4
klyse <Klaus.Pruenster@gmail.com>
2022-02-24 19:46:49 +0100

    first points 🎉

--- GoogleHashCode/Algorithms/SolverK1.cs
+++ GoogleHashCode/Algorithms/SolverK1.cs
@@ -9,6 +9,32 @@ public class SolverK1 : ISolver<Input, Output>
 {
     public Output Solve(Input input)
     {
-       return null;
+        var output = new Output();
+
+        foreach (var project in input.Projects)
+        {
+            var assignment = new Assignment();
+            var notFount = false;
+            foreach (var skill in project.Skills)
+            {
+                var contributor = input.Contributors
+                    .Except(assignment.Contributors)
+                    .FirstOrDefault(c => c.Skills.Any(r => r.Name == skill.Name && r.SkillLevel >= skill.SkillLevel));
+                if (contributor is null)
+                {
+                    notFount = true;
+                    continue;
+                }
+
+                assignment.Contributors.Add(contributor);
+            }
+
+            if (notFount)
+                continue;
+            assignment.Project = project;
+            output.Assignments.Add(assignment);
+        }
+
+        return output;
     }
 }
