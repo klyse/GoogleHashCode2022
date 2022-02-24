70c4a123fdbdb2374524f2d64e8e7453375d6cab
klyse <Klaus.Pruenster@gmail.com>
2022-02-24 19:20:51 +0100

    parsing projects

--- GoogleHashCode/Model/Input.cs
+++ GoogleHashCode/Model/Input.cs
@@ -38,6 +38,28 @@ public record Input(List<Contributor> Contributors, List<Project> Projects)
             }
             contributorsList.Add(new Contributor(name, skillsList));
         }
+        
+        for (var i = 0; i < projects; i++)
+        {
+            var splitRow = values[row].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var name = splitRow[0];
+            var numDays = int.Parse(splitRow[1]);
+            var score = int.Parse(splitRow[2]);
+            var bestBefore = int.Parse(splitRow[3]);
+            var roles = int.Parse(splitRow[4]);
+
+            var skillsList = new List<Skills>();
+            row += 1;
+            for (var y = 0; y < roles; y++)
+            {
+                var splitRow1 = values[row].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var skillName = splitRow1[0];
+                var level = int.Parse(splitRow1[1]);
+                skillsList.Add(new Skills(skillName, level));
+                row += 1;
+            }
+            projectsList.Add(new Project(name, numDays, score, bestBefore, skillsList));
+        }
 
         return new Input(contributorsList, projectsList);
     }
