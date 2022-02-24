078763b274607143f0f1c3827269c3e2394fe0a4
klyse <Klaus.Pruenster@gmail.com>
2022-02-24 18:47:58 +0100

    adjusted files in unit tests

--- Tests/VolkmarsTest.cs
+++ Tests/VolkmarsTest.cs
@@ -10,12 +10,12 @@ public class VolkmarsTest
 {
     [Test]
     [Parallelizable(ParallelScope.All)]
-    [TestCase("a.txt")]
-    [TestCase("b.txt")]
-    [TestCase("c.txt")]
-    [TestCase("d.txt")]
-    [TestCase("e.txt")]
-    [TestCase("f.txt")]
+    [TestCase("a_an_example.in.txt")]
+    [TestCase("b_better_start_small.in.txt")]
+    [TestCase("c_collaboration.in.txt")]
+    [TestCase("d_dense_schedule.in.txt")]
+    [TestCase("e_exceptional_skills.in.txt")]
+    [TestCase("f_find_great_mentors.in.txt")]
     public void Solver2(string example)
     {
         var content = example.ReadFromFile();
